91720e2ce140a05711b22b1cb3edc6372f535c49
Dan Phiffer <dan@phiffer.org>
2014-02-09 13:42:49 -0500

    Sponsors sidebar

--- themes/bos/sidebar.php
+++ themes/bos/sidebar.php
@@ -1,3 +1,11 @@
 <div id="sidebar">
+  <div class="widget">
+    <?php
+    
+    $sponsorship = get_page_by_path('sponsorship');
+    echo apply_filters('the_content', $sponsorship->post_content);
+    
+    ?>
+  </div>
   <?php dynamic_sidebar('sidebar'); ?>
 </div>
